b1932cb628c270af19d5fb0db24ba82ecaca912e
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-09-29 21:32:02 +0000

    1b uses readfile

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-var express = require('express')
-var app = express()
+var http = require("http");
+fs = require('fs');
 
-
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
-
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
-})
+fs.readFile('./public/index.html',function(err,html){
+	if (err){throw err;}
+	http.createServer(function (request, response){
+		response.writeHead(200, {'Content-type': 'text/html'});
+		response.write(html);
+		response.end();
+	}).listen(8080);
+});
---
+++ main.js
@@ -0,0 +1,11 @@
+var fs = require("fs");
+
+fs.readFile('index.html',function(err,html){
+    if(err) return console.error(err);
+    console.log(data.toString());
+
+});
+// var data = fs.readFileSync('index.html');
+
+//console.log(data.toString());
+conslole.log("program ended");
