9bd614fad133143aef3b5f30d88610c9672cd395
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-10-03 22:21:56 +0000

    part 2 uses open and read and stat

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 var http = require("http");
-fs = require('fs');
-var data = fs.readFileSync('./public/index.html');
+var fs = require('fs');
+var path = require('path');
 
-	http.createServer(function (request, response){
-		response.writeHead(200, {'Content-type': 'text/html'});
-		response.write(data);
-		response.end();
-	}).listen(8080);
+fs.open('./public/index.html','r',function(err,file){
+	fs.fstat(file,function(err,stats){
+		var bufferSize=stats.size,
+		chunkSize=512,
+		buffer=new Buffer(bufferSize),
+		bytesRead=0;
+
+		while(bytesRead<bufferSize){
+			if((bytesRead+chunkSize)>bufferSize){
+				chunkSize=(bufferSize-bytesRead);
+			}
+			fs.read(file,buffer,bytesRead,chunkSize,bytesRead);
+			bytesRead += chunkSize;
+		}
+
+//		fs.close(file);
+	
+	});	
+});
+
+		http.createServer(function(request,response){
+			response.writeHead(200,{'Content-type':'text/html'});
+			response.write(file);
+			response.end();
+		}).listen(8080);
