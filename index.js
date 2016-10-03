4d6724775fe48b8dee3d93f062a6a46874e3bc8b
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-10-03 20:46:30 +0000

    1a uses readfilesync

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var http = require("http");
 fs = require('fs');
+var data = fs.readFileSync('./public/index.html');
 
-fs.readFile('./public/index.html',function(err,html){
-	if (err){throw err;}
 	http.createServer(function (request, response){
 		response.writeHead(200, {'Content-type': 'text/html'});
-		response.write(html);
+		response.write(data);
 		response.end();
 	}).listen(8080);
-});
